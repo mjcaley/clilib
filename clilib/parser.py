ca00318e21b9601519fc472979d42feca8fd6696
Michael Caley <mjcaley@darkarctic.com>
2022-03-07 11:22:47 -0500

    Removed redundant attribute

--- clilib/parser.py
+++ clilib/parser.py
@@ -16,7 +16,6 @@ class ParameterCollisionError(Exception):
 class ParameterMapping:
     owner: Any
     name: str
-    parameter_str: str  # Redundant?
     definition: Parameter
 
     def set(self, value: Any):
@@ -31,7 +30,7 @@ def flatten_parameters(parameters: Any) -> dict[str, ParameterMapping]:
 
     for name, param in getattr(parameters, PARAMDEF).items():
         for param_name in param.names:
-            flattened_params[param_name] = ParameterMapping(owner=parameters, name=name, parameter_str=param_name, definition=param)
+            flattened_params[param_name] = ParameterMapping(owner=parameters, name=name, definition=param)
 
     for child_param_attr_name in getattr(parameters, CHILDPARAM).keys():
         child = getattr(parameters, child_param_attr_name)
