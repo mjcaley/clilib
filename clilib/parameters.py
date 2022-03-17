aed4811446fc6d1476a4a3497fd459407f2db777
Michael Caley <mjcaley@darkarctic.com>
2022-03-16 23:55:12 -0400

    Converted parameters to a class

--- clilib/parameters.py
+++ clilib/parameters.py
@@ -1,10 +1,27 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Any, Optional, Type, get_type_hints
+from typing import Any, Optional, Type, cast, get_type_hints
+
+PARAMMETA = "_param_meta_"
+
+
+@dataclass
+class ParameterMeta:
+    option_defs: dict[str, Parameter]
+    argument_defs: dict[str, Parameter]
+    child_params: dict[str, Type]
+    name: Optional[str] = None
+
+
+def is_parameters(obj: Any) -> bool:
+    return hasattr(obj, PARAMMETA)
+
+
+def get_param_meta(parameters: Any) -> ParameterMeta:
+    return getattr(parameters, PARAMMETA)
 
 
 # region Parameter definitions
-
 
 class Parameter:
     def __init__(self, *names: str, default=None, help: str = ""):
@@ -31,86 +48,71 @@ class Flag(Parameter):
 # endregion
 
 
-PARAMMETA = "_param_meta_"
+class Parameters:
+    def __init_subclass__(cls, /, name: str = None, **kwargs):
+        if not name:
+            name = cls.__name__.lower()
 
+        # Future parameter definition
+        options = {}
+        arguments = {}
+        child_parameters = {}
 
-@dataclass
-class ParameterMeta:
-    option_defs: dict[str, Parameter]
-    argument_defs: dict[str, Parameter]
-    child_params: dict[str, Type]
-    name: Optional[str] = None
+        # Collect all the attribute names, values, and types
+        annotations = get_type_hints(cls)
+        cls_vars = vars(cls)
+        attributes = {
+            name: (annotations.get(name, None), cls_vars.get(name, None))
+            for name in annotations.keys() | cls_vars.keys()
+        }
 
+        for attr_name, item in attributes.items():
+            type_, value = item
 
-def is_parameters(obj: Any) -> bool:
-    return hasattr(obj, PARAMMETA)
+            # Copy the parameter definition to the _paramdef dictionary
+            # Assign the default value to the attribute
+            # If there's no type annotation, make the default a string
+            if isinstance(value, Option) or isinstance(value, Flag):
+                options[attr_name] = value
+                annotations.setdefault(attr_name, Optional[str])
+            elif isinstance(value, Argument):
+                arguments[attr_name] = value
+                annotations.setdefault(attr_name, str)
+            # Save child parameter groups
+            elif type_ and issubclass(type_, Parameters):
+                child_parameters[attr_name] = type_
 
+        meta = ParameterMeta(options, arguments, child_parameters, name=name)
+        setattr(cls, PARAMMETA, meta)
+        setattr(cls, "__annotations__", annotations)
 
-def get_param_meta(parameters: Any) -> ParameterMeta:
-    return getattr(parameters, PARAMMETA)
+    def __new__(cls, *args, **kwargs):
+        namespace = dict(vars(cls))
+        meta = get_param_meta(cls)
 
+        options = {}
+        arguments = {}
+        param_children = {}
+        for mro_cls in reversed(cls.__mro__):
+            if not is_parameters(mro_cls):
+                continue
+            meta = get_param_meta(mro_cls)
+            arguments.update(meta.argument_defs)
+            options.update(meta.option_defs)
+            param_children.update(meta.child_params)
 
-def param_init(self, *_, **kwargs):
-    meta = get_param_meta(self)
+        for name, value in {**arguments, **options}.items():
+            if value.default:
+                namespace[name] = value.default  # Set value from default
+            else:
+                namespace[name] = None
 
-    param_defs = {**meta.argument_defs, **meta.option_defs}
+        # Instantiate child parameter groups
+        for name, type_ in param_children.items():
+            child = type_()
+            namespace[name] = child
 
-    for name, value in param_defs.items():
-        if value.default is not None:
-            setattr(self, name, value.default)  # Set value from default
+        namespace[PARAMMETA] = ParameterMeta(options, arguments, param_children, meta.name)
+        instance = type(cls.__name__, (object,), namespace, *args, **kwargs)
 
-    # Instantiate child parameter groups
-    for name, type_ in meta.child_params.items():
-        child = type_()
-        setattr(self, name, child)
-
-
-def parameters(cls: Type) -> Type:
-    # Setup class methods
-    setattr(cls, "__init__", param_init)
-
-    # Future parameter definition
-    options = {}
-    arguments = {}
-    child_parameters = {}
-
-    # Collect all the attribute names, values, and types
-    # TODO: Traverse MRO to get inherited members
-    for mro_cls in reversed(cls.__mro__):
-        if is_parameters(mro_cls):
-            mro_meta = get_param_meta(mro_cls)
-            options.update(mro_meta.option_defs)
-            arguments.update(mro_meta.argument_defs)
-            child_parameters.update(mro_meta.child_params)
-    annotations = get_type_hints(cls)
-    cls_vars = vars(cls)
-    attributes = {
-        name: (annotations.get(name, None), cls_vars.get(name, None))
-        for name in annotations.keys() | cls_vars.keys()
-    }
-
-    for name, item in attributes.items():
-        type_, value = item
-
-        # Copy the parameter definition to the _paramdef dictionary
-        # Assign the default value to the attribute
-        # If there's no type annotation, make the default a string
-        if isinstance(value, Option) or isinstance(value, Flag):
-            options[name] = value
-            setattr(cls, name, None)
-            annotations.setdefault(name, Optional[str])
-        elif isinstance(value, Argument):
-            arguments[name] = value
-            setattr(cls, name, None)
-            annotations.setdefault(name, str)
-
-        # Save child parameter groups
-        elif is_parameters(type_):
-            setattr(cls, name, None)
-            child_parameters[name] = type_
-
-    meta = ParameterMeta(options, arguments, child_parameters, name=None)
-    setattr(cls, PARAMMETA, meta)
-    setattr(cls, "__annotations__", annotations)
-
-    return cls
+        return cast(cls, instance)
--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -4,8 +4,9 @@ from clilib.parameters import (
     Argument,
     Flag,
     Option,
+    Parameters,
+    get_param_meta,
     is_parameters,
-    parameters,
 )
 
 
@@ -41,64 +42,91 @@ def test_flag(help_message):
     assert help_message == f.help
 
 
-def test_is_parameter_true():
-    @parameters
-    class Blank:
-        ...
+def test_parameters_meta():
+    class Child(Parameters): ...
 
-    assert is_parameters(Blank)
+    class Parent(Parameters):
+        name: str = Argument("NAME")
+        age: int = Option("--age")
+        child: Child
+
+    p = Parent()
+    meta = get_param_meta(p)
+
+    assert "parent" == meta.name
+    assert "name" in meta.argument_defs
+    assert Parent.name is meta.argument_defs["name"]
+    assert "age" in meta.option_defs
+    assert Parent.age is meta.option_defs["age"]
+    assert Child is meta.child_params["child"]
+
+
+def test_parameters_name():
+    class Parent(Parameters, name="mike"): ...
+
+    meta = get_param_meta(Parent)
+
+    assert "mike" == meta.name
+
+
+def test_is_parameters_true():
+    class Blank(Parameters): ...
+
+    b = Blank()
+    result = is_parameters(b)
+
+    assert result
 
 
 def test_is_parameters_false():
-    class Blank:
-        ...
+    class Blank: ...
 
-    assert not is_parameters(Blank)
+    b = Blank()
+    result = is_parameters(b)
+
+    assert not result
 
 
-def test_parameter_defaults_set_when_instantiated():
-    @parameters
-    class Person:
-        name: str = Argument("NAME", default="Mike", help="A person's name")
-        age: int = Option("-o", default=42, help="A person's age")
-
-    # Class definition doesn't have default
-    assert None is Person.name
-    assert None is Person.age
+def test_parameter_defaults_to_none():
+    class Person(Parameters):
+        name: str = Argument("NAME")
 
     p = Person()
 
-    # Default set after instantiation
+    assert None is p.name
+
+
+def test_parameter_defaults_set_when_instantiated():
+    class Person(Parameters):
+        name: str = Argument("NAME", default="Mike", help="A person's name")
+        age: int = Option("-o", default=42, help="A person's age")
+
+    p = Person()
+
     assert "Mike" == p.name
     assert 42 == p.age
 
 
 def test_child_parameter():
-    @parameters
-    class Child:
+    class Child(Parameters):
         name: str = Argument("NAME", default="Kara", help="A child's name")
 
-    @parameters
-    class Parent:
+    class Parent(Parameters):
         name: str = Argument("NAME", default="Mike", help="A parent's name")
         child: Child
 
     p = Parent()
 
-    assert isinstance(p.child, Child)
     assert "Kara" == p.child.name
 
 
 def test_inherited_parameters():
-    @parameters
-    class ParentName:
+    class ParentName(Parameters):
         name: str = Argument("NAME", default="Mike")
 
-    @parameters
-    class ChildName:
+    class ChildName(Parameters):
         name: str = Argument("NAME", default="Kara")
 
-    @parameters
     class Person(ParentName, ChildName): ...
 
     p = Person()
